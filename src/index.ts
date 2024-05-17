61213c6d9d360974904f897ea8f4987970b0e315
rodentskie <rodentskie@gmail.com>
2024-05-17 09:04:17 +0800

    Merge pull request #28 from intersective/feat/hsts
    chore: hsts

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,11 @@ import wrap from '@dazn/lambda-powertools-pattern-basic';
 import { Log, error } from './utils';
 import { experience, allExperiences } from './experience';
 import { dashboard } from './dashboard';
-const doNotWaitForEmptyEventLoop = require('@middy/do-not-wait-for-empty-event-loop');
 
+const doNotWaitForEmptyEventLoop = require('@middy/do-not-wait-for-empty-event-loop');
+import h from 'helmet';
+
+const helmet = h as any
 interface Event {
   type: string;
   id: number;
@@ -40,4 +43,11 @@ module.exports.handler = wrap(async (event: Event) => {
   runOnBefore: true,
   runOnAfter: true,
   runOnError: true,
+})).use(helmet({
+  strictTransportSecurity: {
+    maxAge: 31536000, // Must be at least 1 year to be approved
+    includeSubDomains: true, // Must be enabled to be approved
+    preload: true,
+  },
+  xContentTypeOptions: true, // nosniff 
 }));
